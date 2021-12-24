f93e9694e7831866c6635177e0aaa8ef7abe7624
Artem <awrer1234eads@gmail.com>
2021-12-24 21:07:07 +0300

    Update api key

--- js/urls.js
+++ js/urls.js
@@ -1,11 +1,11 @@
 export const URLS = {
   WEATHER_URL: {
     SERVER_URL: 'https://api.openweathermap.org/data/2.5/weather',
-    API_KEY: 'f660a2fb1e4bad108d6160b7f58c555f&units=metric',
+    API_KEY: '38809edaf50d2af3b78dd8016e4f9ef4&units=metric',
   },
   FORECAST_URL: {
     SERVER_URL: 'https://api.openweathermap.org/data/2.5/forecast',
-    API_KEY: 'f660a2fb1e4bad108d6160b7f58c555f&units=metric',
+    API_KEY: '38809edaf50d2af3b78dd8016e4f9ef4&units=metric',
   },
   ICON_URL: 'https://openweathermap.org/img/wn/',
 }
